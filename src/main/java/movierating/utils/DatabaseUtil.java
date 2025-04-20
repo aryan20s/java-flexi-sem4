5bda899cc6b2469c0a96ae4dd22de4eda9fbd759
Bhoomika DJ <bhoomsdj@gmail.com>
2025-04-21 02:09:55 +0530

    db integration enter password into DatabaseUtil.java

--- src/main/java/movierating/utils/DatabaseUtil.java
+++ src/main/java/movierating/utils/DatabaseUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/movie?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root"; // Replace with your MySQL username
-    private static final String PASSWORD = "Chi@050505"; // Replace with your MySQL password
+    private static final String PASSWORD = "Enter Password here"; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
